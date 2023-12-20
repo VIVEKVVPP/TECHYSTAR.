796ea88ffe0ee2ae12171617e21a72287f2ee469
Vivek patel <96973804+VIVEKVVPP@users.noreply.github.com>
2023-12-20 19:37:31 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/vivek-patel-57165b233?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target={"blank"}>
+            Linkedin
+          </a>
+          <a href="https://www.instagram.com/vivek__patel_17?igshid=YzVkODRmOTdmMw==" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/VIVEKVVPP" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
